67dad5f18413b0750e32a3961769ed2741953db2
Daniel Lewis <thecontrarian42@gmail.com>
2017-04-16 19:30:52 -0500

    changed to indices, got worse

--- tensor_translator.py
+++ tensor_translator.py
@@ -16,18 +16,22 @@ def vector_char_map(alphabet):
     span, vector = bit_vectorizer(alphabet)
     return {vector(index).data.hex(): alphabet[index] for index in span}
 
+def char_index_map(alphabet):
+    " returns a map of characters to indices in alphabet list"
+    return {alphabet[index]: index for index in range(len(alphabet))}
+
 class TensorTranslator:
     " build a tensor([bit array]) <-> string translator "
 
     def to_tensor(self, string):
-        " convert a string in the target alphabet to an array of bit vectors "
-        return [self._char_vector_map[c] for c in string]
+        " convert a string in the target alphabet to an array of indices "
+        return [self._char_index_map[c] for c in string]
 
     def to_string(self, tensor):
         " convert a bit vector built with this translator back to a string "
         return ''.join([
-            self._vector_char_map[vector.data.hex()] for vector in tensor])
+            self._alphabet[index] for index in tensor])
 
     def __init__(self, alphabet):
-        self._char_vector_map = char_vector_map(alphabet)
-        self._vector_char_map = vector_char_map(alphabet)
+        self._char_index_map = char_index_map(alphabet)
+        self._alphabet = alphabet
